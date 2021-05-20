Transpose saliency map of wrong dimension

code/data/salient_kinetics.py
@@ -160,11 +160,14 @@ class SalientKinetics400(Kinetics400):
 
         # Scale saliency to size of video if they are not the same
         if saliency.shape[-2:] != video.shape[1:3]:
-            # HACK: I accidentally transposed one dataset partially which does not need interpolation
+            # HACK: I accidentally transposed one dataset partially which 
+            # does not need interpolation but it does need transposing
             vid_size = torch.tensor(video.shape[1:3]) 
             sal_size = torch.tensor(saliency.shape[-2:])
 
-            if not (vid_size == torch.flip(sal_size, [0])).all():
+            if (vid_size == torch.flip(sal_size, [0])).all():
+                saliency = saliency.permute(0, 2, 1)
+            else:
                 video_path = self.video_clips.metadata['video_paths'][video_idx]
                 correct_size = (video.shape[1], video.shape[2])
                 saliency = torch.nn.functional.interpolate(saliency, size=correct_size)